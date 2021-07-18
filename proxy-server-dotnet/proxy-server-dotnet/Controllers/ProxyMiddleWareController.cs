61a9ca76063f1ff31e536c0a9236922aa52bd5a5
Phatcharaporn <zphatcharaporn.a@pttdigital.com>
2021-07-18 17:40:32 +0700

    [Add] proxy middle ware

---
+++ proxy-server-dotnet/proxy-server-dotnet/Controllers/ProxyMiddleWareController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace proxy_server_dotnet.Controllers
+{
+    [ApiController]
+    [Route("api/")]
+    public class ProxyMiddleWareController : ControllerBase
+    {
+
+        [HttpGet]
+        [Route("A")]
+        public ActionResult<string> GetPublicIpA()
+        {
+            return "value";
+        }
+
+        [HttpGet]
+        [Route("B")]
+        public ActionResult<string> GetPublicIpB()
+        {
+            return "value";
+        }
+
+    }
+}
